e761e98c612561d278d607dd625721c3dc03b211
CamiloG <78550048+CamiloGuevaraEsp@users.noreply.github.com>
2025-05-16 18:38:47 -0400

    Update clean_dead_MAC.m
    Solved an issue where the code did not read the first rows if they were empty

--- clean_dead_MAC.m
+++ clean_dead_MAC.m
@@ -1,19 +1,22 @@
 clear all
-rootdir      = "/Users/camilog/Documents/Camilo/Sleep_codes_MAC/20250414";
-filename     = "20250414__ZT0to24_multiColumn.xlsx";
+rootdir      = "/Users/camilog/Downloads/";
+filename     = "250516_SD__ZT0to24_multiColumn.xlsx";
 NumDays = 3; 
 
 cd(rootdir)
 output       = filename + "_edited.xlsx";
 
 
-activity_sheet = readtable(filename,'Sheet','Activity Counts Per min');
+activity_sheet = readcell(filename,'Sheet','Activity Counts Per min','Range','A1');
+activity_sheet(cellfun(@(x) all(ismissing(x)), activity_sheet)) = {NaN};
+data_sheet2 = cell2mat(activity_sheet(2:end,2:end));              ;
+%activity_sheet = readtable(filename,'Sheet','Activity Counts Per min','Range', 'A1','ReadVariableNames', false);
 
 
-data_sheet2 = activity_sheet{:,vartype('numeric')};
+%data_sheet2 = activity_sheet{:,vartype('numeric')};
 dead_flies = (isnan(data_sheet2));
-dead_flies = find(any(dead_flies,2));
-dead_flies_days = dead_flies+1;
+dead_flies = find(any(dead_flies,2)) ;
+dead_flies_days = dead_flies + 1 ;
 
 sleep_days_index = [1,8:7+(NumDays-1)];
 other_index = [2:7];
@@ -28,12 +31,17 @@ for i = sleep_days_index
 end
 
 for i = other_index
-    data = readtable(filename, 'Sheet',i);
-    rows_to_delete = dead_flies; 
-    data(rows_to_delete,:) = [];
-    writetable(data, output, 'Sheet', char(sheetNames{i}));
+    data = readcell(filename,'Sheet',i,'Range','A1');
+    data(cellfun(@(x) all(ismissing(x)), data)) = {NaN};
+    rows_to_keep = setdiff(1:size(data,1),dead_flies);
+    data = data(rows_to_keep,:);
+    %data = readtable(filename, 'Sheet',i);
+    %rows_to_delete = dead_flies; 
+    %data(rows_to_delete,:) = [];
+    writecell(data, output, 'Sheet', char(sheetNames{i}), 'WriteMode','append');
 end
 
+writematrix(dead_flies, output, 'Sheet', 'Dead Flies');
 
 function sheetNames = getExcelSheetNames(filename)
 %GETEXCELSHEETNAMES Extracts sheet names from a .xlsx file (cross-platform)
@@ -71,6 +79,7 @@ function sheetNames = getExcelSheetNames(filename)
     % Clean up
     rmdir(tempDir, 's');
 end
+
 
 
 
